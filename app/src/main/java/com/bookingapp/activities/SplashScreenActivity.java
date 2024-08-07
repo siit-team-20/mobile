e8d46fea0c2d93bebb153b443a4a4a421406e443
Andro <andrija.slovic1@gmail.com>
2024-08-07 16:29:37 +0200

    added splash screen activity

---
+++ app/src/main/java/com/bookingapp/activities/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.bookingapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.bookingapp.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        int SPLASH_TIME_OUT = 3000;
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
