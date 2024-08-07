4761699fbe389219cb6bf91185060c3d29e2cf45
Andro <andrija.slovic1@gmail.com>
2024-08-07 16:52:57 +0200

    added navigation and drawer

--- app/src/main/java/com/bookingapp/activities/HomeActivity.java
+++ app/src/main/java/com/bookingapp/activities/HomeActivity.java
@@ -1,20 +1,141 @@
 package com.bookingapp.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.bookingapp.R;
+import com.bookingapp.databinding.ActivityHomeBinding;
 import com.bookingapp.fragments.FragmentTransition;
 import com.bookingapp.fragments.accommodation.AccommodationListFragment;
 import com.bookingapp.fragments.accommodation.AccommodationsPageFragment;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class HomeActivity extends AppCompatActivity {
+    private ActivityHomeBinding binding;
+    private AppBarConfiguration mAppBarConfiguration;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private NavController navController;
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Set<Integer> topLevelDestinations = new HashSet<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        FragmentTransition.to(AccommodationsPageFragment.newInstance(), HomeActivity.this, false, R.id.activity_main);
+        Log.i("BookingApp", "HomeActivity onCreate()");
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        binding.activityHomeBase.floatingActionButton.setOnClickListener(v -> {
+//            Log.i("BookingApp", "Floating Action Button");
+//            Intent intent = new Intent(HomeActivity.this, CartActivity.class);
+//            intent.putExtra("title", "Cart");
+//            startActivity(intent);
+//        });
+
+        drawer = binding.drawerLayout;
+        navigationView = binding.navView;
+        toolbar = binding.activityHomeBase.toolbar;
+
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(false);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_hamburger);
+            actionBar.setHomeButtonEnabled(true);
+        }
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+//        topLevelDestinations.add(R.id.nav_language);
+//        topLevelDestinations.add(R.id.nav_settings);
+
+        navController = Navigation.findNavController(this, R.id.fragment_nav_content_main);
+        navController.addOnDestinationChangedListener((navController, navDestination, bundle) -> {
+            Log.i("BookingApp", "Destination Changed");
+            int id = navDestination.getId();
+            boolean isTopLevelDestination = topLevelDestinations.contains(id);
+
+            if (!isTopLevelDestination) {
+                switch (id) {
+                    case 1000005:
+                        navController.navigate(R.id.nav_accommodations);
+                        //Toast.makeText(HomeActivity.this, "Products", Toast.LENGTH_SHORT).show();
+                        break;
+//                    case R.id.nav_new:
+//                        Toast.makeText(HomeActivity.this, "New product", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case R.id.nav_profile:
+//                        Toast.makeText(HomeActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case R.id.nav_logout:
+//                        Toast.makeText(HomeActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+//                        break;
+                }
+                drawer.closeDrawers();
+            }
+//            else {
+//                switch (id) {
+//                    case R.id.nav_settings:
+//                        Toast.makeText(HomeActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case R.id.nav_language:
+//                        Toast.makeText(HomeActivity.this, "Language", Toast.LENGTH_SHORT).show();
+//                        break;
+//                }
+//            }
+        });
+
+        mAppBarConfiguration = new AppBarConfiguration
+                .Builder(R.id.nav_accommodations)
+                //R.id.nav_new, R.id.nav_profile, R.id.nav_logout, R.id.nav_settings, R.id.nav_language
+                .setOpenableLayout(drawer)
+                .build();
+
+        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        navController = Navigation.findNavController(this, R.id.fragment_nav_content_main);
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
+    }
+
+    public boolean onSupportNavigateUp() {
+        navController = Navigation.findNavController(this, R.id.fragment_nav_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+
 }
